84f5af27ad358dffc88d142316a091e0677ee21f
asharpc <asarudheenpc@yaoo.com>
2019-06-14 11:37:50 +0530

    form data save into database

--- landing/home/forms.py
+++ landing/home/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, ValidationError
 
+from .models import EmailSignUp
+
 class LandingForm(FlaskForm):
 	full_name = StringField('Full name',
 		render_kw={"class": "form-control",
@@ -23,6 +25,15 @@ class LandingForm(FlaskForm):
 				)
 		])
 
+	def validate_full_name(self, field):
+		obj = EmailSignUp.query.filter_by(full_name=field.data).first()
+		if obj is not None:
+			raise ValidationError('take another username!')
+			
 	def validate_email(self, field):
 		if field.data.endswith(".edu"):
 			raise ValidationError("You cannot use a school email address.")
+		obj = EmailSignUp.query.filter_by(email=field.data).first()
+		if obj is not None:
+			msg = 'this email is already taken'
+			raise ValidationError(msg)	
--- landing/home/views.py
+++ landing/home/views.py
@@ -1,27 +1,39 @@
 from landing import app
-from flask import render_template,request
+from flask import render_template,request,redirect
 import json
 
 
 
 from .forms import LandingForm
 from .models import EmailSignUp
+
 @app.route("/home/", methods=['GET','POST'])
 def home():
-	# form = LandingForm()
-	# print("json",request.data)
-	# print(request.form)
-	# if request.method == 'POST':
-	# 	data = request.stream.read().decode('utf-8')
-	# 	print(data)
-		# final_data = json.load(data)
-		# print(final_data['hello'])
 	form = LandingForm()
 	if form.validate_on_submit():
 		data = form.data
 		print(data)
 		if 'csrf_token' in data:
 			del data['csrf_token']
-		obj = EmailSignUp(**data)
-		# obj.save()
+		obj = EmailSignUp.query.filter_by(email=form.email.data).first()
+		if obj is None:
+			obj = EmailSignUp(**data)
+			obj.save()
+		form = LandingForm()
+		return render_template('home.html',form=form)
 	return render_template('home.html', form=form)
+
+@app.route("/item/<int:id>/", methods=['GET'])
+def item_detail(id):
+	instance = EmailSignUp.query.filter_by(id=id).first_or_404()
+	return render_template('items/detail.html', instance=instance)
+
+
+@app.route("/item/<int:id>/update/", methods=['GET','POST'])
+def item_update(id):
+	instance = EmailSignUp.query.filter_by(id=id).first_or_404()
+	form = LandingForm(obj=instance)
+	if form.validate_on_submit():
+		data = fom.data
+		print(data)
+	return render_template('items/form.html', instance=instance, form=form)
