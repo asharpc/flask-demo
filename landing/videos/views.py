afeae2becbe05fa711aa011f777544dee271c9a4
asharpc <asarudheenpc@yaoo.com>
2019-06-18 17:47:30 +0530

    video detail page

--- landing/videos/views.py
+++ landing/videos/views.py
@@ -3,12 +3,16 @@ from flask import render_template, redirect, request, url_for
 
 from .models import Video
 
-from landing import static
 
-import os
 
 @app.route("/video/", methods=['GET','POST'])
 def video_list():
 	obj = Video.query.all()
 	image_file = url_for('static', filename='video_thumbs/')
 	return render_template("video/list.html",instance=obj, image_file=image_file)
+
+@app.route("/video/<int:id>/", methods=['GET'])
+def video_detail(id):
+	obj = Video.query.filter_by(id=id).first_or_404()
+	image_file = url_for('static', filename='video_thumbs/')
+	return render_template("video/detail.html",instance=obj,image_file=image_file)
